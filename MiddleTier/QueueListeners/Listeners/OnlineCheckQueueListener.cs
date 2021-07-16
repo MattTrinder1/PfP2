743bdf295259a313e6b97880a20949e64e5e116d
Paul Thomas <Paul.Thomas@tisski.com>
2021-07-16 08:56:36 +0000

    Merged PR 1024: Add OnlineCheckQueueListener
    Add OnlineCheckQueueListener

---
+++ MiddleTier/QueueListeners/Listeners/OnlineCheckQueueListener.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace QueueListeners
+{
+    public class OnlineCheckQueueListener
+    {
+        [Function("OnlineCheckQueueListener")]
+        public void Run([QueueTrigger("onlinecheckqueue")] string myQueueItem, FunctionContext context)
+        {
+            var log = context.GetLogger("OnlineCheckQueueListener");
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+    }
+}
