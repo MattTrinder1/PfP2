7cdb3fc7e4976046c26dc12b5dcb62cb29c0f381
Matt Trinder <matt.trinder@tisski.com>
2021-09-15 18:47:41 +0100

    attempt trigger

--- MiddleTier/QueueListeners/Listeners/PNBQueueListener.cs
+++ MiddleTier/QueueListeners/Listeners/PNBQueueListener.cs
@@ -34,12 +34,12 @@ namespace QueueListeners
             string newQueueItem = Helpers.ParseQueueMessage(myQueueItem);
             var docRoot = JsonDocument.Parse(newQueueItem).RootElement;
 
-            log.LogInformation($"\t mapping to business entities");
+            log.LogInformation($"\tmapping to business entities");
             PocketNotebook pocketNotebook = _mapper.Map<PocketNotebook>(docRoot);
             pocketNotebook.Photos = _mapper.Map<List<Photo>>(docRoot.GetProperty("photos").EnumerateArray());
 
 
-            log.LogInformation($"\t calling API");
+            log.LogInformation($"\tcalling API");
             RestRequest req = Helpers.GetRestRequest("/api/pnb", Method.POST, docRoot.GetProperty("enteredBy").GetString() );
             req.AddJsonBody(pocketNotebook, "application/json");
             var resp = _restClient.Execute(req);
