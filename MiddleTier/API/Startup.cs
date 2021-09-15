ecdc2a45dabb449713e4393cb4b3a73440b3b3fa
Matt Trinder <matt.trinder@tisski.com>
2021-09-15 12:40:52 +0100

    change data access factory to scoped rather than singleton

--- MiddleTier/API/Startup.cs
+++ MiddleTier/API/Startup.cs
@@ -50,7 +50,7 @@ namespace API
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-            services.AddSingleton<DVDataAccessFactory>();
+            services.AddScoped<DVDataAccessFactory>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
