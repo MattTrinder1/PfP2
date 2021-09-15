15eb06d20690f57a6fa3fa06c8b188abc0e188f1
Matt Trinder <matt.trinder@tisski.com>
2021-09-15 13:02:16 +0100

    uniqyue instance back to false

--- MiddleTier/DataverseRepository/DVDataAccessFactory.cs
+++ MiddleTier/DataverseRepository/DVDataAccessFactory.cs
@@ -103,7 +103,7 @@ namespace API.DataverseAccess
                 if (dvService == null || !(dvService as ServiceClient).IsReady)
                 {
                     Uri uri = new Uri(connectionConfiguration.DVUrl);
-                    dvService = new ServiceClient(uri, connectionConfiguration.ClientId, connectionConfiguration.ClientSecret, true);
+                    dvService = new ServiceClient(uri, connectionConfiguration.ClientId, connectionConfiguration.ClientSecret, false);
                 }
             
 #else
@@ -152,7 +152,7 @@ namespace API.DataverseAccess
 #if MOCKUP
             userServiceClient = crm.CreateOrganizationService(impersonationId);
 #else
-            userServiceClient = new ServiceClient(uri, connectionConfiguration.ClientId, connectionConfiguration.ClientSecret, true);
+            userServiceClient = new ServiceClient(uri, connectionConfiguration.ClientId, connectionConfiguration.ClientSecret, false);
             (userServiceClient as ServiceClient).CallerAADObjectId = GetUserADObjectId(impersonationEmailAddress);
 #endif
             
