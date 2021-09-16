58f57c95425f23976ef3aef349a75082ae956ae9
Paul Thomas <Paul.Thomas@tisski.com>
2021-09-16 17:19:33 +0100

    Fixed image handling following refactor of DV model classes. Use of DVImage attribute is now optional as any (DV class) byte[] properties are assumed to represent image data unless attributed with DVNotImage.

---
+++ MiddleTier/Common/Models/Attributes/DVNotImageAttribute.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// DV properties of type byte array (byte[]) are assumed to represent image data unless attributed with DVNotImageAttribute.
+/// </summary>
+[System.AttributeUsage(System.AttributeTargets.Property, AllowMultiple = false)]
+public class DVNotImageAttribute : System.Attribute
+{
+    public DVNotImageAttribute()
+    {
+    }
+}
--- MiddleTier/DataverseRepository/DVDataAccessFactory.cs
+++ MiddleTier/DataverseRepository/DVDataAccessFactory.cs
@@ -16,26 +16,7 @@ namespace API.DataverseAccess
 {
     public class DVDataAccessFactory
     {
-        //ASSUMPTIONS:
-        //* ServiceClient is thread safe.
-        //* ServiceClient implements its own connection caching internally.
-        //  So invoking 'new ServiceClient' does not mean a new connection will be created unless 'useUniqueInstance' is set to true.
-        //* Creating a ServiceClient with useUniqueInstance (true) is much more time consuming as it forces reauthentication.
-        //* So overriding ServiceClient's caching (by useUniqueInstance) or thread safety (by DisableCrossThreadSafeties) should not be 
-        //  required aside from exceptional circumstances which should not apply to this API.
-        //* User context is achieved by setting CallerAADObjectId on a ServiceClient instance.
-        //  As ServiceClient should be thread safe, it may be possible to set this inline (within GetUserDataAccess) on a single ServiceClient 
-        //  instance shared across the factory/API/threads but documentation is not clear on this so to be explicit/safe, we create a ServiceClient
-        //  instance per CallerAADObjectId.
-        //  (Remembering that due to internal caching, ServiceClient may be reusing a connection anyway even though we are creating 
-        //   individual ServiceClient instances to hold each CallerAADObjectId.)
-        //* Documentation is also not clear on how long a ServiceClient instance is valid for or likewise if there is significant overhead in 
-        //  always instantiating new ServiceClient instances (even when useUniqueInstance is false) so again to be explicit/safe we cache each 
-        //  ServiceClient instance for a specific amount of time.
         private IOrganizationService dvService = null;
-
-        private const int userDvServiceCacheSlidingExpirationMinutes = 240;
-        private const int userDvServiceCacheAbsoluteExpirationMinutes = 720;
 
         private ConnectionConfiguration connectionConfiguration = null;
         private IMemoryCache cache = null;
