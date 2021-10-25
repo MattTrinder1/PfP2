9b8b7d09f28aa1d1504200892fa651d2147031e2
Matt Trinder <matt.trinder@tisski.com>
2021-10-25 18:27:08 +0100

    filter TPA for current customer

--- MiddleTier/Common/Models/Business/LookupField.cs
+++ MiddleTier/Common/Models/Business/LookupField.cs
@@ -9,6 +9,10 @@ namespace Common.Models.Business
             Values = new List<LookupValue>();
         }
 
+        public LookupField(List<LookupValue> values)
+        {
+            Values = new List<LookupValue>(values);
+        }
         public string DisplayName { get; set; }
 
         public List<LookupValue> Values { get; set; }
---
+++ MiddleTier/Common/Models/Dataverse/DVCustomerSetting.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using System;
+
+namespace Common.Models.Dataverse
+{
+    [EntityLogicalName("cp_customersetting")]
+    public class DVCustomerSetting : DVEntityBase
+    {
+        public DVCustomerSetting()
+        {
+            this.LogicalName = "cp_customersetting";
+        }
+
+        public Guid? cp_customersettingid { get { return base.Id; } set { base.Id = value.Value; } }
+
+        public string cp_customername { get { return this.GetAttributeValue<string>(nameof(cp_customername)); } set { this.Attributes[nameof(cp_customername)] = value; } }
+        public bool cp_active { get { return this.GetAttributeValue<bool>(nameof(cp_active)); } set { this.Attributes[nameof(cp_active)] = value; } }
+        public string cp_incidentprefix { get { return this.GetAttributeValue<string>(nameof(cp_incidentprefix)); } set { this.Attributes[nameof(cp_incidentprefix)] = value; } }
+        public string cp_policingareatext { get { return this.GetAttributeValue<string>(nameof(cp_policingareatext)); } set { this.Attributes[nameof(cp_policingareatext)] = value; } }
+    }
+}
