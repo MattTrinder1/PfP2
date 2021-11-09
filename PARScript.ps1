96f7b570d0a4637dc3f7e6c3a3e4d117d30453b6
Matt Trinder <matt.trinder@tisski.com>
2021-11-09 17:05:32 +0000

    Covid App

---
+++ PARScript.ps1
@@ -0,0 +1,24 @@
+﻿$pwd="viWUCJJD4YgAlUXpaVJ@R54C"
+$userName = "cumbriapolice"
+$apiKey = "OeJHkYTswJK8Rlun5T5X6pIequYj0lC5XlsaT1xj"
+
+$token = Invoke-WebRequest `
+            -Method POST `
+            -Uri  https://uat.driver-vehicle-licensing.api.gov.uk/thirdparty-access/v1/authenticate `
+            -Body (@{"userName"=$userName;"password"=$pwd}|ConvertTo-Json) `
+
+
+$jwt = (ConvertFrom-Json( $token.Content)).psobject.properties.value
+
+Write-Host $jwt
+
+$driverNumber = "AMRSH652170JX8PD"
+
+Invoke-WebRequest `
+            -Method POST `
+            -Uri  https://uat.driver-vehicle-licensing.api.gov.uk/driver-photo-at-the-roadside/v1/drivers/driver-details/s `
+            -Headers @{"Authorization" = $jwt;"x-api-key" =$apiKey} `
+            -Body (@{"driverNumber"=$driverNumber}|ConvertTo-Json) `
+            -ContentType application/json
+
+
