fb29f10c0948e48119dc22a9579a624a4982f3cf
Matt Trinder <matt.trinder@tisski.com>
2021-11-12 15:41:23 +0000

    add durham live

--- PipelineTools/FlowRewriter/Program.cs
+++ PipelineTools/FlowRewriter/Program.cs
@@ -29,6 +29,9 @@ namespace FlowRewriter
             j["properties"]["definition"]["actions"]["Populate_a_Microsoft_Word_template"]["inputs"]["parameters"]["file"] = "areibfsdfdas";
             Save("../../../RewrittenFlows/Cumbria/Live/Schema/FlowsPDFCP/unpacked/Workflows/GenerateCovidPDFs-67CA08CD-5D8D-EB11-B1AC-000D3ADB469C.json", j.ToString().Replace("'", @"\u0027"));
 
+            j["properties"]["definition"]["actions"]["Populate_a_Microsoft_Word_template"]["inputs"]["parameters"]["file"] = "areibfsdfdas";
+            Save("../../../RewrittenFlows/Durham/Live/Schema/FlowsPDFCP/unpacked/Workflows/GenerateCovidPDFs-67CA08CD-5D8D-EB11-B1AC-000D3ADB469C.json", j.ToString().Replace("'", @"\u0027"));
+
 
             Console.WriteLine("Covid PDF FLow done");
 
