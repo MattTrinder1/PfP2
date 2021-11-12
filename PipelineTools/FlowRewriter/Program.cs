36bc08bc4621b9289c68ce16b06e173c032f0bc3
Matt Trinder <matt.trinder@tisski.com>
2021-11-12 16:58:39 +0000

    rewrite cumbria UAT UOF Flow

--- PipelineTools/FlowRewriter/Program.cs
+++ PipelineTools/FlowRewriter/Program.cs
@@ -47,7 +47,18 @@ namespace FlowRewriter
             Console.WriteLine("Covid PDF FLow done");
 
 
+            Console.WriteLine("Rewriting UoF PDF FLow");
+            json = File.ReadAllText("../../../../../Schema/FlowsUseOfForceCP/unpacked/Workflows/GenerateUoFPDF-BDAE81A8-29EE-EB11-BACB-0022489C5E28.json");
+            j = JObject.Parse(json);
+            j["properties"]["definition"]["actions"]["Populate_a_Microsoft_Word_template"]["inputs"]["parameters"]["source"] = "me";
+            j["properties"]["definition"]["actions"]["Populate_a_Microsoft_Word_template"]["inputs"]["parameters"]["drive"] = "b!k32VxZOE5U-HwWt6Ey23yGSuL7xVALpHlDcSd7uGMIo67u-9iOaWRrln1WJDTm3I";
+            j["properties"]["definition"]["actions"]["Populate_a_Microsoft_Word_template"]["inputs"]["parameters"]["file"] = "01JPJUSCPQL76JG7BTPVG2OGRLZTRSDHLU";
+            Save("../../../RewrittenFlows/Cumbria/UAT/Schema/FlowsUseOfForce/unpacked/Workflows/GenerateUoFPDF-BDAE81A8-29EE-EB11-BACB-0022489C5E28.json", j.ToString().Replace("'", @"\u0027"));
+
+
         }
+
+
 
 
         private static void Save(string filePath, string contents)
